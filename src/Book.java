e15f2a9bf5a5b7440b40f0d277baba78822a817e
Elena32387 <iea20ster@gmail.com>
2024-12-10 15:50:57 +0300

    ДЗ 7 вторая часть

--- src/Book.java
+++ src/Book.java
@@ -27,6 +27,25 @@ public class Book {
         this.publishingYear = publishingYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(publishingYear, book.publishingYear);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publishingYear);
+    }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", publishingYear=" + publishingYear +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ public class Main {
     public static void main(String[] args) {
     Author rand = new Author("Айн", "Рэнд");
     Author verne = new Author("Жюль", "Верн");
+
     Book theFountainhead = new Book("Источник", rand, 1943);
+    Book theFountainhead2 = new Book("Источник", rand, 1943);
     Book theMysteriousIsland = new Book("Таинственный остров", verne, 1874);
 
-        System.out.println(theFountainhead.getTitle()+ " " +theFountainhead.getPublishingYear());
-        System.out.println(theMysteriousIsland.getTitle()+ " " +theMysteriousIsland.getPublishingYear());
+        System.out.println(theFountainhead);
+        System.out.println(theMysteriousIsland);
+
+        System.out.println("Сравление theFountainhead "+ theFountainhead.equals(theFountainhead2));
 
         theFountainhead.setPublishingYear(1948);
-        System.out.println(theFountainhead.getTitle()+ " " +theFountainhead.getPublishingYear());
+
+        System.out.println(theFountainhead);
+        System.out.println("Сравление theFountainhead "+ theFountainhead.equals(theFountainhead2));
 
 
     }
